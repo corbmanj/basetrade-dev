40497f4fff5ac246f990781e33b3fff143aea923
Jordan Corbman <jordan.corbman@macmillan.com>
2017-04-26 08:44:12 -0500

    added text for support page and media queries for navbar

--- modules/Support.js
+++ modules/Support.js
@@ -2,6 +2,18 @@ import React, {Component} from 'react'
 
 export default class Support extends Component{
   render () {
-    return <span>Support us</span>
+    return (
+			<div style={{width: '90%', margin: 'auto'}}>
+				<h2>How can I help BaseTradeTV?</h2>
+				<p>BaseTradeTV is committed to supporting StarCraft as a global eSport. The only way we can continue to be successful is through your continued viewership and support.</p>
+				<p>By supporting BaseTradeTV you are helping to create tournaments and support players worldwide. Without your help, BaseTrade would not be able to participate in amazing endeavors like our team house, player sponsorships, and weekly tournaments. Thank you!</p>
+			<h3>So what can you do to help?</h3>
+			<ul>	<li>Follow BaseTradeTV on Twitch and Twitter</li>
+				<li>Subscribe to BaseTradeTV either directly or through Amazon Prime</li>
+				<li>Tell your friends about BaseTradeTV</li>
+				<li>Keep being awesome!</li>
+			</ul>
+			</div>
+		)
   }
 }
