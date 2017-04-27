4fa91d9d1fbd38e4bd461730d71d5f790d02b4ed
Jordan Corbman <jordan.corbman@macmillan.com>
2017-04-26 20:17:54 -0500

    add stream iframe

--- modules/Stream.js
+++ modules/Stream.js
@@ -8,11 +8,19 @@ export default class Stream extends Component{
   render () {
     return (
       <div>
-        <a href="https://api.twitch.tv/kraken/oauth2/authorize?response_type=code&client_id=mafllvn2ycrixc59ygrh12orgg03p4&redirect_uri=http://localhost:8080/authCallback&scope=viewing_activity_read
+			{/* <a href="https://api.twitch.tv/kraken/oauth2/authorize?response_type=code&client_id=mafllvn2ycrixc59ygrh12orgg03p4&redirect_uri=http://localhost:8080/authCallback&scope=viewing_activity_read
       &state=950g9loz5s9jsvnm861g4aizw74h6y">
           Click here to see the live stream
         </a>
-        <div>{this.props.params.code}</div>
+        <div>{this.props.params.code}</div> */}
+				<iframe
+    			src="http://player.twitch.tv/?channel=baseTradeTV"
+    			height="100%"
+					width="100%"
+					frameborder="0"
+					scrolling="no"
+					allowfullscreen="true">
+				</iframe>
       </div>
     )
   }
